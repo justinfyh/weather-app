2480dfb28a80b6ef98eaf1b1f43c93438b6d2b19
justin <justinhuang2020@gmail.com>
2023-11-25 00:35:37 +1300

    Add listener for enter key main page

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import '../App.css';
@@ -7,13 +7,19 @@ import '../App.css';
 
 const Main = () => {
     const [city, setCity] = useState('');
+    let navigate = useNavigate();
 
+    const handleEnter = (event) => {
+        if (event.key === 'Enter') {
+            navigate(`/weather/${city}`);
+        }
+    };
 
     return (
         <div className="App-header">
             {/* <header > */}
                 <p>Weather App</p>
-                <input type="text" placeholder="Enter city" value={ city } onChange={(e) => setCity(e.target.value)}/>
+                <input type="text" placeholder="Enter city" value={ city } onKeyDown={handleEnter} onChange={(e) => setCity(e.target.value)}/>
                 <Link to={`/weather/${city}`} className="button"><FontAwesomeIcon icon={faArrowRight} /></Link>
             {/* </header> */}
         </div>
