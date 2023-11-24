190ab10ecdf81a85b698c812061948b283c8bc86
justin <justinhuang2020@gmail.com>
2023-11-24 22:33:08 +1300

    Fix api issues

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,35 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const Weather = () => {
     const [city, setCity] = useState('');
+    const [latitude, setLatitude] = useState('');
+    const [longtitude, setLongtitude] = useState('');
     const [weatherData, setWeatherData] = useState(null);
 
-    // useEffect (() => {
-    //     setCity('Auckland');
-    // }, []);
+    // const getLocation = async () => {
+    //     const apiKey = 'f138a38b8516bedbc16e2966e3d59a4a';
+    //     const apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`;
+
+    //     try {
+    //         const response = await fetch(apiUrl);
+    //         const data = await response.json();
+    //         setLatitude(data.lat);
+    //         setLongtitude(data.lon);
+    //     } catch (error) {
+    //         console.error('Error getting city:', error);
+    //     }
+    // }
 
     const getWeatherData = async () => {
         const apiKey = 'f138a38b8516bedbc16e2966e3d59a4a';
-        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}`;
+        // const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${ latitude }&lon=${ longtitude }&appid=${apiKey}`;
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
         try {
             const response = await fetch(apiUrl);
             const data = await response.json();
             setWeatherData(data);
           } catch (error) {
-            console.error('Error fetching weather data:', error);
+            console.error('Error getting weather data:', error);
           }
+    }
+
+    const getData = () => {
+        // getLocation();
+        getWeatherData();
     }
 
     return (
         <div className="Weather-container">
             <h1>Weather</h1>
-            <button onClick={ getWeatherData }>Get Weather</button>
+            <input type="text" placeholder="Enter city" value={ city } onChange={(e) => setCity(e.target.value)}/>
+
+            <button onClick={ getData }>Get Weather</button>
 
             {weatherData && (
             <div>
                 <h3>{weatherData.name}</h3>
-                <p>25 degrees</p>
+                <p>25 degrees { latitude }</p>
             </div>
             )}
         </div>
