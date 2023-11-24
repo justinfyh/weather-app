a2dda3fd16865e8a156fd47c758c42b9085b97a2
justin <justinhuang2020@gmail.com>
2023-11-24 21:48:19 +1300

    Implement weather page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './components/Main';
+import Weather from './components/Weather';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path='/' exact Component={ Main } />
+        <Route path='/weather' exact Component={ Weather } />
       </Routes>
     </Router>
   );
---
+++ src/components/Weather.js
@@ -0,0 +1,18 @@
+import React from "react";
+import App from "../App";
+
+const Weather = () => {
+
+    return (
+        <div className="Weather-container">
+            <h1>Weather</h1>
+
+            <div>
+                <h3>Auckland</h3>
+                <p>25 degrees</p>
+            </div>
+        </div>
+    )
+}
+
+export default Weather;
