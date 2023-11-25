182191e32459d2b1812bb7d455d32e40e71f2815
justin <justinhuang2020@gmail.com>
2023-11-25 15:37:53 +1300

    Setup server proxy

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.get('/api/weather', async (req, res) => {
     }
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
+  });
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
